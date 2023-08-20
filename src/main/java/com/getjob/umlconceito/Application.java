3917d8bed03a1629b715c996f1e1fcd4a2542903
getrojob <getrojob@gmai.com>
2023-08-19 21:46:12 -0300

    feat: add instacias das novas classes e seus respectivas injeções

--- src/main/java/com/getjob/umlconceito/Application.java
+++ src/main/java/com/getjob/umlconceito/Application.java
@@ -1,30 +1,76 @@
 package com.getjob.umlconceito;
 
-import com.getjob.umlconceito.domain.Categoria;
-import com.getjob.umlconceito.repositories.CategoriaRepository;
+import com.getjob.umlconceito.domain.*;
+import com.getjob.umlconceito.domain.enums.TipoCliente;
+import com.getjob.umlconceito.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner{
+public class Application implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(1,"Eletronico");
-		Categoria cat2 = new Categoria(2,"Informatica");
-		Categoria cat3 = new Categoria(3,"Escritório");
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Eletronico");
+        Categoria cat2 = new Categoria(null, "Informatica");
+        Categoria cat3 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p2.getCategorias().addAll(Arrays.asList(cat1));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "1312331233", TipoCliente.PESSOAFISICA);
+        cli1.getTelefones().addAll(Arrays.asList("123123123", "6534561234"));
+
+        Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "123123123", cli1, c1);
+        Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "123123123", cli1, c2);
+
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+    }
 }
