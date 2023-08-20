412dcd0bd41b72d668badc78ead89aafbae2f2bb
getrojob <getrojob@gmai.com>
2023-08-19 21:42:43 -0300

    fix: add JsonManagedReference para tratar Infinite Recursion e relacionamento entre as classes produto e categoria

--- src/main/java/com/getjob/umlconceito/domain/Categoria.java
+++ src/main/java/com/getjob/umlconceito/domain/Categoria.java
@@ -1,8 +1,11 @@
 package com.getjob.umlconceito.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -12,9 +15,13 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @JsonManagedReference
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
     }
@@ -42,6 +49,14 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -54,4 +69,5 @@ public class Categoria implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
